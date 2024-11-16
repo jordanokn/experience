7a6e942b46abba7f1dc66eede3ddb170c7422bd8
George Knyazyan <jorj.knyazyan.15@gmail.com>
2024-11-16 23:06:17 +0300

    cleaner

---
+++ cleaner/__main__.py
@@ -0,0 +1,37 @@
+import click
+
+
+def remove_comment(line: str, all_comments: bool = True) -> str:
+    """ пока не работает разделение на all_comments """
+    comment_index = line.find('#')
+    if comment_index != -1:
+        if not any(quote in line[:comment_index] for quote in ('"', "'")):
+            stripped_line = line[:comment_index].rstrip()
+            if stripped_line:
+                return stripped_line + '\n'
+            return stripped_line
+    return line
+
+
+@click.command()
+@click.option("--all_comments", default=True, help="что хочешь???")
+@click.option("--file", help="мне вот откуда удалять коментарии а, щас все снесу?")
+def clean_file_comments(
+    file: str,
+    all_comments: bool = True
+):
+    if not file:
+        raise ValueError("Option --file required")
+    
+    with open(file, "r", encoding='utf-8') as f:
+        lines = f.readlines()
+
+    cleaned_lines = [remove_comment(line, all_comments) for line in lines]
+    
+    with open(file, "w", encoding='utf-8') as f:
+        f.writelines(cleaned_lines)
+
+
+if __name__ == "__main__":
+    clean_file_comments()
+    
